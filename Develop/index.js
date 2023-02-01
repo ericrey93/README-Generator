a0e7d90acb0d6d4be6986652280bb9a7f8cd7bbd
Eric Reyes <ericreyes1109@gmail.com>
2023-01-31 20:37:38 -0600

    created the code on both javascript files to create a README file

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,62 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+// I created the README format on a seperate file, and am pulling it here
+const generateRead = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer.prompt([
+    // I included the most important questions that should be shown on any professional README
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your projects/apps title?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Describe your project/app.',
+    },
+    {
+        type: 'input',
+        name: 'installaton',
+        message: 'Explain any installation process.',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Explain the usage of the project.',
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'Which license will be used? Please type one of these three: MIT, Eclipse or Mozilla',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'How can someone contribute to your project?',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Type your tests, if any, for your project.',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Please enter your github username.',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter your email address.',
+    },
+])
+// this function pulls all of the user's inputs and pushes them onto the README file that will be generated
+.then((answers) => {
+    const readmeContent = generateRead(answers);
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+    fs.writeFile("README.md", readmeContent, (err) => 
+        err ? console.log(err): console.log("README has been generated!"));
+});
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,75 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  const icons = {
+    MIT: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
+    Eclipse: '[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)',
+    Mozilla: '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)',
+  }
+  return icons[license]
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  const iconLinks = {
+    MIT: 'https://opensource.org/licenses/MIT',
+    Eclipse: 'https://opensource.org/licenses/EPL-1.0',
+    Mozilla: 'https://opensource.org/licenses/MPL-2.0',
+}
+return iconLinks[license]
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license) {
+    return `Licensed under the ${renderLicenseLink(license)}`
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
 
+// tagged every area that will be linked with user input with a parameter to pull values.
+  return `
+  # ${data.title}
+
+  ${renderLicenseBadge(data.license)}
+
+  ## Table of Content
+   - [Project description](#Description)
+   - [Installation](#Installation)
+   - [Usage](#Usage)
+   - [License](#License)
+   - [Contributing](#Contributing)
+   - [Test](#Test)
+   - [Questions](#Questions)
+
+   ## Description
+   ${data.description}
+
+   ## Installation
+   ${data.installation}
+
+   ## Usage
+   ${data.usage}
+
+   ## License
+   ${renderLicenseSection(data.license)}
+
+
+   ## Contributing
+   ${data.contributing}
+
+   ## Test
+   ${data.test}
+
+   ## Questions
+   ${data.github}
+   ${data.email}
 `;
 }
 
